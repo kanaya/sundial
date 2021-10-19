b5610585de2338cadda66f074ffca603f78d3b5f
Ichi Kanaya <kanaya@pineapple.cc>
2021-10-19 14:28:43 +0900

    Trial 1.

--- Firmware/Sundial/Sundial.ino
+++ Firmware/Sundial/Sundial.ino
@@ -12,28 +12,33 @@ int step_num = 0;
 int delta = 1;
 int color[] = {255, 255, 0};
 
-int hourToHue(int h, int i) {
+uint32_t hourToRGB(int h, int i) {
   // 0 <= h < 2400
   // 0 <= i < N_Pixels
   int index = h * N_Pixels / 2400;
-  int hue;
+  int sunness;  // 0 to 255
+  int rgb;
   if (i == index) {
-    hue = 0;
+    sunness = 255;
   }
   else {
-    hue = 65536L / 2;
+    sunness = 0;
   }
-  return hue;
+  int r = sunness;
+  int g = sunness / 2;
+  int b = 255 - sunness;
+  return pixels.Color(r, g, b);
 }
 
 void setup() {
   pixels.begin();
+  pixels.show();
 }
 
 void loop() {
   for (int i = 0; i < pixels.numPixels(); ++i) {
-    int pixelHue = hourToHue(hour, i);
-    pixels.setPixelColor(i, pixels.gamma32(pixels.ColorHSV(pixelHue)));
+    uint32_t pixelColor = hourToRGB(hour, i);
+    pixels.setPixelColor(i, pixels.gamma32(pixelColor));
   }
   pixels.show();
   delay(Delay);
