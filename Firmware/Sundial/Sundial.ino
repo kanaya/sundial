23255addf44b8ed6e87ca987cb483ccf63807e49
Ichi Kanaya <kanaya@pineapple.cc>
2021-10-19 20:49:14 +0900

    Looks good.

--- Firmware/Sundial/Sundial.ino
+++ Firmware/Sundial/Sundial.ino
@@ -6,7 +6,7 @@ const int Delay = 10;
 
 Adafruit_NeoPixel pixels(N_Pixels, Signal_Pin, NEO_GRB + NEO_KHZ800);
 
-int hour = 600; // 0 to 2400
+int hour = 0; // 0 to 2400
 
 int step_num = 0;
 int delta = 1;
@@ -14,32 +14,37 @@ int color[] = {255, 255, 0};
 
 uint32_t hourToRGB(int h, int i) {
   // 0 <= h < 2400
-  // 0 <= i < N_Pixels
-  int index = h * N_Pixels / 2400;
-  int sunness;  // 0 to 255
-  int rgb;
-  if (i == index) {
-    sunness = 255;
-  }
-  else {
-    sunness = 0;
-  }
-  int r = sunness;
-  int g = sunness / 2;
-  int b = 255 - sunness;
+
+  Serial.print("h = ");
+  Serial.print(h);
+  Serial.print(", i = ");
+  Serial.println(i);
+
+  float j = float(i) / float(N_Pixels);
+  float k = float(h) / 2400.0;
+  float l = k - j;
+  
+  float sunness = exp(-l * l * 24.0 * 24.0);
+  
+  int r = int(255.0 * sunness);
+  int g = int(255.0 * sunness);
+  int b = int(255.0 * sunness);
   return pixels.Color(r, g, b);
 }
 
 void setup() {
+  Serial.begin(9600);
   pixels.begin();
   pixels.show();
 }
 
 void loop() {
-  for (int i = 0; i < pixels.numPixels(); ++i) {
-    uint32_t pixelColor = hourToRGB(hour, i);
-    pixels.setPixelColor(i, pixels.gamma32(pixelColor));
+  for (hour = 0; hour < 2400; ++hour) {
+    for (int i = 0; i < pixels.numPixels(); ++i) {
+      uint32_t pixelColor = hourToRGB(hour, i);
+      pixels.setPixelColor(i, pixels.gamma32(pixelColor));
+    }
+    pixels.show();
+    delay(Delay);
   }
-  pixels.show();
-  delay(Delay);
 }
